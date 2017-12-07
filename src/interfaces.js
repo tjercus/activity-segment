9241caaf1f62958cf7b1411073cb4864275bcf49
Tjerk Valentijn <tjerk.valentijn@inology.nl>
2017-12-07 15:16:02 +0100

    added impl and tests

---
+++ src/interfaces.js
@@ -0,0 +1,16 @@
+
+/**
+ * @interface Segment
+ * @property {string} uuid
+ * @property {string} trainingUuid - the parent object
+ * @property {number} distance - ex: 18.5
+ * @property {string} duration - ex "01:12:59" (HH:mm:ss)
+ * @property {string} pace - ex: "04:23" (mm:ss)
+ */
+
+/**
+ * @interface Total
+ * @property {number} distance
+ * @property {string} duration
+ * @property {string} pace
+ */
